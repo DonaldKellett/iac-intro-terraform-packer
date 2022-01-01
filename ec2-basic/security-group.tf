696a51f104c87fe3af3261cb0ca17c9f6f280bf4
Donald Sebastian Leung <donaldsebleung@gmail.com>
2022-01-01 19:56:03 +0800

    Add security group for ec2-basic

---
+++ ec2-basic/security-group.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "ec2_basic" {
+  name        = "ec2_basic"
+  description = "Security group for basic EC2 instance"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description      = "SSH from anywhere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
